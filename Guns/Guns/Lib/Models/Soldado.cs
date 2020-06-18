8db00ccda32f123f6113007838382d6bf4dea67b
Jordi <jordi.contreras@gmail.com>
2020-06-18 10:17:25 +0200

    Soldado class created with revolver, rifle and escopeta Interfaces

---
+++ Guns/Guns/Lib/Models/Soldado.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guns.Lib.Models
+{
+    public class Soldado
+    {
+        protected IArma arma;
+
+        public Soldado(IArma _arma)
+        {
+            this.arma = _arma;
+        }
+
+        public string Disparar()
+        {
+            return this.arma.Disparar();
+        }
+    }
+
+    public interface IArma
+    {
+        string Disparar();
+    }
+
+    public class Revolver : IArma
+    {
+        public string Disparar()
+        {
+            return "Pum Pum ..";
+        }
+    }
+
+    public class Rifle : IArma
+    {
+        public string Disparar()
+        {
+            return "Pum pum pum pum pum ..";
+        }
+    }
+
+    public class Escopeta : IArma
+    {
+        public string Disparar()
+        {
+            return "pum PUMMM !! ..";
+        }
+    }
+}
---
+++ Guns/Guns/Program.cs
@@ -0,0 +1,23 @@
+﻿using Guns.Lib.Models;
+using System;
+
+namespace Guns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //soldado shoots with revolver
+            var soldado = new Soldado(new Revolver());
+            Console.WriteLine(soldado.Disparar());
+
+            //soldado2 shoots with escopeta
+            var soldado2 = new Soldado(new Escopeta());
+            Console.WriteLine(soldado2.Disparar());
+
+            //soldado3 shoots with rifle
+            var soldado3 = new Soldado(new Rifle());
+            Console.WriteLine(soldado3.Disparar()); 
+        }
+    }
+}
